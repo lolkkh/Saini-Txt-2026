6dba74b0a72a0785e9ba304c86345b1b109f406e
lolkkh <sujalchavdax@gmail.com>
2026-07-21 01:48:15 +0900

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -5,13 +5,13 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "22182189"))
 API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8287212718:AAEPfAZ5Nbar3TTBM6uTz4fyh7HYqLjP8XE")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "7934179213"))
-CREDIT = environ.get("CREDIT", "Engineers Babu")
+OWNER = int(environ.get("OWNER", "8392347882"))
+CREDIT = environ.get("CREDIT", "admin")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '7934179213').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '8392347882').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
 AUTH_USER = os.environ.get('AUTH_USERS', '7934179213').split(',')
